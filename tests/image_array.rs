feb06760f4b1f9b86690d506da85836e5067cabc
Graham Wihlidal <graham@wihlidal.ca>
2018-08-26 19:56:19 +0200

    Combine array tests into non-array files

--- tests/image_array.rs
+++
@@ -1,4 +0,0 @@
-//! Tests are based on the common synchronization examples on the Vulkan-Docs wiki: https://github.com/KhronosGroup/Vulkan-Docs/wiki/Synchronization-Examples.
-
-extern crate ash;
-extern crate vk_sync;
